0b02e5622ae4581b71dfd7d34a689553779f019a
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2021-06-11 18:14:36 -0500

    Add files via upload

---
+++ dashboard/precache-manifest.670c28d70ec93aa1c358b6ef9d1e4a45.js
@@ -0,0 +1,490 @@
+self.__precacheManifest = [
+  {
+    "revision": "3307e65500d07b30339a",
+    "url": "/css/app.ee32d8ae.css"
+  },
+  {
+    "revision": "3307e65500d07b30339a",
+    "url": "/js/app.2605cdfd.js"
+  },
+  {
+    "revision": "978cacb947f7b6aabd88",
+    "url": "/js/chunk-vendors.0f6dc5d0.js"
+  },
+  {
+    "revision": "596223f7bff9598c852e949e9d2b1aa0",
+    "url": "/img/lato-700.596223f7.svg"
+  },
+  {
+    "revision": "a35757dab5ff64a5ab9e39e684e6ed09",
+    "url": "/img/comfortaa-v23-latin-600.a35757da.svg"
+  },
+  {
+    "revision": "54d3cfe0cec734925a21cebfe378a12e",
+    "url": "/img/lato-300.54d3cfe0.svg"
+  },
+  {
+    "revision": "1babbf161b484449b90090b068ab8442",
+    "url": "/img/playfair-display-700.1babbf16.svg"
+  },
+  {
+    "revision": "02e026b5ddf7001ae32e99b7a147d5c9",
+    "url": "/img/playfair-display-400.02e026b5.svg"
+  },
+  {
+    "revision": "98d8cf792834c0bef59c2be99dc3533d",
+    "url": "/fonts/lato-300.98d8cf79.woff2"
+  },
+  {
+    "revision": "656963c4beb814e754e38478fcaa0439",
+    "url": "/fonts/lato-300.656963c4.woff"
+  },
+  {
+    "revision": "1b8f8afa9b4ba60fbcf9936eae918ec6",
+    "url": "/fonts/lato-300.1b8f8afa.eot"
+  },
+  {
+    "revision": "483a27c988018f92133995b7d1453248",
+    "url": "/fonts/lato-300.483a27c9.ttf"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/fonts/lato-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/fonts/lato-700.874b8e7b.woff"
+  },
+  {
+    "revision": "fe7d53e4cac8f5b946a9e25f004efc8c",
+    "url": "/fonts/lato-700.fe7d53e4.ttf"
+  },
+  {
+    "revision": "dfa15e750be6bc53dfbfcd14c61188fe",
+    "url": "/fonts/playfair-display-400.dfa15e75.eot"
+  },
+  {
+    "revision": "f9203469f750b4284c62aa49f9496cb0",
+    "url": "/fonts/lato-700.f9203469.eot"
+  },
+  {
+    "revision": "496b0ee7c32c16da8c6289fa8aae5268",
+    "url": "/fonts/playfair-display-400.496b0ee7.woff2"
+  },
+  {
+    "revision": "e4a38c67e9051d378cc441113289d23b",
+    "url": "/fonts/playfair-display-400.e4a38c67.woff"
+  },
+  {
+    "revision": "df1d9fe12078f6d528730d21f8a653f8",
+    "url": "/fonts/playfair-display-700.df1d9fe1.woff2"
+  },
+  {
+    "revision": "2ec4c26d8d4b3ba3766d0c686d558e23",
+    "url": "/fonts/playfair-display-700.2ec4c26d.eot"
+  },
+  {
+    "revision": "6862a76759e8d1c68411c23ac44948f7",
+    "url": "/fonts/playfair-display-700.6862a767.woff"
+  },
+  {
+    "revision": "f47d42dd80b6fc4c1bf437b89230575b",
+    "url": "/fonts/playfair-display-700.f47d42dd.ttf"
+  },
+  {
+    "revision": "5dbbdd652a8dfed1d07c04c63a8a02d6",
+    "url": "/fonts/playfair-display-400.5dbbdd65.ttf"
+  },
+  {
+    "revision": "7eaf8c9ae517e908e953c6daf3563a65",
+    "url": "/fonts/comfortaa-v23-latin-600.7eaf8c9a.woff"
+  },
+  {
+    "revision": "74f97c0bb64e93036f41ab940ef47497",
+    "url": "/fonts/comfortaa-v23-latin-600.74f97c0b.eot"
+  },
+  {
+    "revision": "33a85dd5754c4782b8de7c517e4852be",
+    "url": "/fonts/comfortaa-v23-latin-600.33a85dd5.ttf"
+  },
+  {
+    "revision": "ace49f92f017b339538ac9b584cf76a7",
+    "url": "/fonts/comfortaa-v23-latin-600.ace49f92.woff2"
+  },
+  {
+    "revision": "b275e2d0457b8833aa7f20c9a35ed4c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74f97c0bb64e93036f41ab940ef47497",
+    "url": "/assets/fonts/comfortaa/comfortaa-v23-latin-600.eot"
+  },
+  {
+    "revision": "a35757dab5ff64a5ab9e39e684e6ed09",
+    "url": "/assets/fonts/comfortaa/comfortaa-v23-latin-600.svg"
+  },
+  {
+    "revision": "33a85dd5754c4782b8de7c517e4852be",
+    "url": "/assets/fonts/comfortaa/comfortaa-v23-latin-600.ttf"
+  },
+  {
+    "revision": "7eaf8c9ae517e908e953c6daf3563a65",
+    "url": "/assets/fonts/comfortaa/comfortaa-v23-latin-600.woff"
+  },
+  {
+    "revision": "ace49f92f017b339538ac9b584cf76a7",
+    "url": "/assets/fonts/comfortaa/comfortaa-v23-latin-600.woff2"
+  },
+  {
+    "revision": "1b8f8afa9b4ba60fbcf9936eae918ec6",
+    "url": "/assets/fonts/lato/lato-300.eot"
+  },
+  {
+    "revision": "54d3cfe0cec734925a21cebfe378a12e",
+    "url": "/assets/fonts/lato/lato-300.svg"
+  },
+  {
+    "revision": "483a27c988018f92133995b7d1453248",
+    "url": "/assets/fonts/lato/lato-300.ttf"
+  },
+  {
+    "revision": "656963c4beb814e754e38478fcaa0439",
+    "url": "/assets/fonts/lato/lato-300.woff"
+  },
+  {
+    "revision": "98d8cf792834c0bef59c2be99dc3533d",
+    "url": "/assets/fonts/lato/lato-300.woff2"
+  },
+  {
+    "revision": "f9203469f750b4284c62aa49f9496cb0",
+    "url": "/assets/fonts/lato/lato-700.eot"
+  },
+  {
+    "revision": "596223f7bff9598c852e949e9d2b1aa0",
+    "url": "/assets/fonts/lato/lato-700.svg"
+  },
+  {
+    "revision": "fe7d53e4cac8f5b946a9e25f004efc8c",
+    "url": "/assets/fonts/lato/lato-700.ttf"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/assets/fonts/lato/lato-700.woff"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/assets/fonts/lato/lato-700.woff2"
+  },
+  {
+    "revision": "dfa15e750be6bc53dfbfcd14c61188fe",
+    "url": "/assets/fonts/playfair-display/playfair-display-400.eot"
+  },
+  {
+    "revision": "02e026b5ddf7001ae32e99b7a147d5c9",
+    "url": "/assets/fonts/playfair-display/playfair-display-400.svg"
+  },
+  {
+    "revision": "5dbbdd652a8dfed1d07c04c63a8a02d6",
+    "url": "/assets/fonts/playfair-display/playfair-display-400.ttf"
+  },
+  {
+    "revision": "e4a38c67e9051d378cc441113289d23b",
+    "url": "/assets/fonts/playfair-display/playfair-display-400.woff"
+  },
+  {
+    "revision": "496b0ee7c32c16da8c6289fa8aae5268",
+    "url": "/assets/fonts/playfair-display/playfair-display-400.woff2"
+  },
+  {
+    "revision": "2ec4c26d8d4b3ba3766d0c686d558e23",
+    "url": "/assets/fonts/playfair-display/playfair-display-700.eot"
+  },
+  {
+    "revision": "1babbf161b484449b90090b068ab8442",
+    "url": "/assets/fonts/playfair-display/playfair-display-700.svg"
+  },
+  {
+    "revision": "f47d42dd80b6fc4c1bf437b89230575b",
+    "url": "/assets/fonts/playfair-display/playfair-display-700.ttf"
+  },
+  {
+    "revision": "6862a76759e8d1c68411c23ac44948f7",
+    "url": "/assets/fonts/playfair-display/playfair-display-700.woff"
+  },
+  {
+    "revision": "df1d9fe12078f6d528730d21f8a653f8",
+    "url": "/assets/fonts/playfair-display/playfair-display-700.woff2"
+  },
+  {
+    "revision": "8d063bad016654b0be7c3f8c9048ad53",
+    "url": "/assets/images/fallback.svg"
+  },
+  {
+    "revision": "db37489cce87107de1204b95c4f7f2c0",
+    "url": "/assets/images/patterns/bubbles.svg"
+  },
+  {
+    "revision": "6e7dcc65dd9f29e772ddd8efd98cca54",
+    "url": "/assets/images/patterns/circles.svg"
+  },
+  {
+    "revision": "6df8f5ae809331f7bbf1ede34fd1edb5",
+    "url": "/assets/images/patterns/cogs.svg"
+  },
+  {
+    "revision": "672b3b0cbf09cd08639d9bc2223e9dca",
+    "url": "/assets/images/patterns/anchors.svg"
+  },
+  {
+    "revision": "56e23500708541ca3404cdf609974458",
+    "url": "/assets/images/patterns/circuit.svg"
+  },
+  {
+    "revision": "3ea7197475a9e02d7c9d38916a9671df",
+    "url": "/assets/images/patterns/bathroom.svg"
+  },
+  {
+    "revision": "5936ede168c208b036187b72d478728c",
+    "url": "/assets/images/patterns/brick.svg"
+  },
+  {
+    "revision": "9707858e197e589b16e26050f5f86cd0",
+    "url": "/assets/images/patterns/dots.svg"
+  },
+  {
+    "revision": "3dbb5eebde27a308df336003cb6ae682",
+    "url": "/assets/images/patterns/food.svg"
+  },
+  {
+    "revision": "ab66f7882f2694a3008ed2179bd1acd6",
+    "url": "/assets/images/patterns/formal.svg"
+  },
+  {
+    "revision": "ab38b7abf54328e172d582ce5c5204e4",
+    "url": "/assets/images/patterns/hideout.svg"
+  },
+  {
+    "revision": "cd2019e1f473fada047869c197be86e4",
+    "url": "/assets/images/patterns/glamorous.svg"
+  },
+  {
+    "revision": "47269e5c2fa18aac87014d3345ba423e",
+    "url": "/assets/images/patterns/leaf.svg"
+  },
+  {
+    "revision": "138d29c89c76ff0b6cb881ea37a521bc",
+    "url": "/assets/images/patterns/jigsaw.svg"
+  },
+  {
+    "revision": "f897399e0608f5de26a80d32be38e634",
+    "url": "/assets/images/patterns/overlapping-circles.svg"
+  },
+  {
+    "revision": "cce42d3a0bf64fc11e22032c1e06d0d6",
+    "url": "/assets/images/patterns/overcast.svg"
+  },
+  {
+    "revision": "ebdf51b14eea09fee037cdc2044a6b02",
+    "url": "/assets/images/patterns/plus.svg"
+  },
+  {
+    "revision": "c817d740d70bf5866eb51f0e7ae838c8",
+    "url": "/assets/images/patterns/shapes.svg"
+  },
+  {
+    "revision": "4fbada2ce7ce7c9e29dc2b7411809edd",
+    "url": "/assets/images/patterns/skulls.svg"
+  },
+  {
+    "revision": "8d743bd584c2d144a2d9a41df198d7f9",
+    "url": "/assets/images/patterns/star.svg"
+  },
+  {
+    "revision": "195f38ce20601bccb88607a803599c63",
+    "url": "/assets/images/patterns/texture.svg"
+  },
+  {
+    "revision": "54ebad5332dc12b2a32dfd2dea8c91c3",
+    "url": "/assets/images/patterns/stripes.svg"
+  },
+  {
+    "revision": "6b5e122177bb4866748b8b52c6867ce0",
+    "url": "/assets/images/patterns/wiggle.svg"
+  },
+  {
+    "revision": "8709816a4186992610b3d928983752f1",
+    "url": "/assets/images/patterns/tic-tac-toe.svg"
+  },
+  {
+    "revision": "bf73eaeee4763f1a99b2440aa4d0c1b4",
+    "url": "/assets/images/patterns/yyy.svg"
+  },
+  {
+    "revision": "a392e5dcece655823225c3fbb41aeeb3",
+    "url": "/assets/images/projects/about-me.svg"
+  },
+  {
+    "revision": "7b89f4b6d395f012b30fa7115b14936b",
+    "url": "/assets/images/patterns/triangles.svg"
+  },
+  {
+    "revision": "8d063bad016654b0be7c3f8c9048ad53",
+    "url": "/assets/images/projects/fallback.svg"
+  },
+  {
+    "revision": "71d69214c15b5736fdf161405aafbf4e",
+    "url": "/assets/images/projects/docs.png"
+  },
+  {
+    "revision": "a753b22cf34895af7696ef4a5d6442d3",
+    "url": "/assets/images/projects/fgv.jpg"
+  },
+  {
+    "revision": "2a6754b4e57bab409f7eb579606d7e44",
+    "url": "/assets/images/projects/bonc.jpg"
+  },
+  {
+    "revision": "2ffd7528b6fc31687497cd920b2eef89",
+    "url": "/assets/images/projects/costco.jpg"
+  },
+  {
+    "revision": "4c441d616d116e8d71b5f9ed1852a8b6",
+    "url": "/assets/images/projects/main-site.png"
+  },
+  {
+    "revision": "c2d389c146b4d78bb74bb03815c1950b",
+    "url": "/assets/images/projects/LoL.png"
+  },
+  {
+    "revision": "9eefda880cb3e50d93f9a1584dc3ea7a",
+    "url": "/assets/images/projects/responsive-snake.png"
+  },
+  {
+    "revision": "2137ead02e4741fe5f8b33bdb43fe42d",
+    "url": "/assets/images/projects/mountain-top.png"
+  },
+  {
+    "revision": "92679f33c1c6893b484209bbee59cc4f",
+    "url": "/assets/images/social-media/github.svg"
+  },
+  {
+    "revision": "3877d69ef35bc33e0240594eab67a36b",
+    "url": "/assets/images/social-media/facebook.svg"
+  },
+  {
+    "revision": "69e13d03b08300d302cf36bbe0879a8b",
+    "url": "/assets/images/projects/nct.png"
+  },
+  {
+    "revision": "35a48caf6b9c0fed29ba1de160254926",
+    "url": "/assets/images/social-media/linkedin.svg"
+  },
+  {
+    "revision": "18c4f8a60be5e35235dbf826d0fdd5f4",
+    "url": "/assets/images/social-media/gitlab.svg"
+  },
+  {
+    "revision": "0cfebbce8087f892567a851d535aa421",
+    "url": "/assets/images/projects/shrink-me.svg"
+  },
+  {
+    "revision": "c7585c96104f7e598dceaec793f7778e",
+    "url": "/assets/images/social-media/twitter.svg"
+  },
+  {
+    "revision": "0ec63f6547ae7e8fc3772971516e45dd",
+    "url": "/assets/images/projects/nctimes.png"
+  },
+  {
+    "revision": "231435c0365aacb18eeec76f93051bf5",
+    "url": "/assets/images/projects/map.png"
+  },
+  {
+    "revision": "55988ece857d91c6b64f7e7cbca1cfa7",
+    "url": "/assets/logo/android-chrome-192x192.png"
+  },
+  {
+    "revision": "873340a3a5e1e2e2d325e0d8724fa257",
+    "url": "/assets/logo/android-chrome-256x256.png"
+  },
+  {
+    "revision": "29e3b186aaaf66c662aece7b6b635c6f",
+    "url": "/assets/logo/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "c8e435a2d9f47c40b831b5e062904d59",
+    "url": "/assets/images/social-media/xing.svg"
+  },
+  {
+    "revision": "bc9137e65f93a7a5dd85540585bc84ad",
+    "url": "/assets/logo/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "341a67c538d67f9ce92005cf14255dc2",
+    "url": "/assets/images/social-media/instagram.svg"
+  },
+  {
+    "revision": "718a811f8c4a747885956b89f33e2fbb",
+    "url": "/assets/logo/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "6c9b5f924f56539f64bdade374d1b8ae",
+    "url": "/assets/logo/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "fa9c41355926814fa7dcea3613068275",
+    "url": "/assets/logo/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "ae839975090f05ca70b137b3ca668de8",
+    "url": "/assets/logo/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "a25d23f55608319e2e4a67da0a386088",
+    "url": "/assets/logo/favicon-16x16.png"
+  },
+  {
+    "revision": "7bfb51b138346406ad7aee7ecce33d4d",
+    "url": "/assets/logo/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "6c9b5f924f56539f64bdade374d1b8ae",
+    "url": "/assets/logo/apple-touch-icon.png"
+  },
+  {
+    "revision": "7f2b2f8a4c6863cc7be0a1e4b7963bd9",
+    "url": "/assets/logo/browserconfig.xml"
+  },
+  {
+    "revision": "0ca09d02cd0a3cc7e7f7bc6aed9e3c37",
+    "url": "/assets/logo/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "6d1fe7217b85c450f19b90dd0cffa268",
+    "url": "/assets/logo/mstile-150x150.png"
+  },
+  {
+    "revision": "c61e094f82323ccff799495eb192ceb8",
+    "url": "/assets/logo/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "44fe19b82676cc49550804ef880e2e3e",
+    "url": "/assets/logo/site.webmanifest"
+  },
+  {
+    "revision": "5df4a2f49c0179506d4bdf822c050c77",
+    "url": "/assets/logo/favicon-32x32.png"
+  },
+  {
+    "revision": "6316c58fee472b52625b93244f5d2db5",
+    "url": "/assets/logo/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "68ced48d1bb1f06560c7a4515f5523fb",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "595d18bc774f8c89e1752ea04a4b7721",
+    "url": "/assets/logo/showroom.svg"
+  }
+];
