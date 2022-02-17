83f4cf643a5f766b8d56ecfd88afea25945533dd
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-02-17 16:49:42 -0600

    Fixed Scrolling

--- js/navbar.js
+++ js/navbar.js
@@ -59,14 +59,14 @@ jQuery(($) => {
 
     // Toggle .header-scrolled class to #header when page is scrolled
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 25) {
             $('#header').addClass('header-scrolled');
         } else {
             $('#header').removeClass('header-scrolled');
         }
     });
 
-    if ($(window).scrollTop() > 100) {
+    if ($(window).scrollTop() > 25) {
         $('#header').addClass('header-scrolled');
     }
 
