73e48138199dfe848bcaf20a6dad56086b5f6624
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2024-02-19 15:26:04 -0500

    Added logos of nc

---
+++ logos/js/colormode.js
@@ -0,0 +1,14 @@
+/*
+function colormode() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+  document.body.classList.add("dark-mode");
+} else {
+  document.body.classList.remove("dark-mode");
+}
+*/
